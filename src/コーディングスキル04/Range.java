67c19c3573352e5146c9d406b5bf765769e2fa6c
fishking0308 <49540933+fishking0308@users.noreply.github.com>
2019-07-10 11:10:01 +0900

    Add files via upload

---
+++ src/コーディングスキル04/Range.java
@@ -0,0 +1,17 @@
+package コーディングスキル04;
+
+public class Range<T extends Comparable> {
+    public T lowerBound;
+    public T upperBound;
+	/**
+	 * Constructor
+	 */
+    public Range(T lowerBound, T upperBound) {
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+    }
+
+    public boolean includes(T value) {
+        return lowerBound.compareTo(value) <= 0 && value.compareTo(upperBound) <= 0;
+    }
+}
